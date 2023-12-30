af5635c42321dd3fe14b63d2c3bf4dbd1ab4021f
Mishaal <152169951+dmishaalk@users.noreply.github.com>
2023-12-30 13:54:38 +0530

    Exploratory data analysis assignment 1

---
+++ plot2.R
@@ -0,0 +1,35 @@
+#Setting directory
+setwd("~/Desktop/R files/ExploratoryDataAnalysis")
+
+#Creating directory
+if(!dir.exists("data")) { dir.create("data") }
+
+#Downloading data
+file.url   <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+file.path  <- "data/household_power_consumption.zip"
+file.unzip <- "data/household_power_consumption.txt"
+if(!file.exists(file.path) & !file.exists(file.unzip)) {
+  download.file(file.url, file.path)
+  unzip(file.path, exdir = "data")
+}
+
+#Loading data
+data <- read.table("data/household_power_consumption.txt", header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+summary(data)
+
+#Subsetting the data from 2007-02-01 and 2007-02-02
+subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+globalActivePower <- as.numeric(subsetdata$Global_active_power)
+globalReactivePower <- as.numeric(subsetdata$Global_reactive_power)
+voltage <- as.numeric(subsetdata$Voltage)
+subMetering1 <- as.numeric(subsetdata$Sub_metering_1)
+subMetering2 <- as.numeric(subsetdata$Sub_metering_2)
+subMetering3 <- as.numeric(subsetdata$Sub_metering_3)
+
+#Creating Time-series, plot2.png
+datetime <- strptime(paste(subsetdata$Date, subsetdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+plot(datetime, globalActivePower, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
+
+#Saving plot
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,38 @@
+#Setting directory
+setwd("~/Desktop/R files/ExploratoryDataAnalysis")
+
+#Creating directory
+if(!dir.exists("data")) { dir.create("data") }
+
+#Downloading data
+file.url   <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+file.path  <- "data/household_power_consumption.zip"
+file.unzip <- "data/household_power_consumption.txt"
+if(!file.exists(file.path) & !file.exists(file.unzip)) {
+  download.file(file.url, file.path)
+  unzip(file.path, exdir = "data")
+}
+
+#Loading data
+data <- read.table("data/household_power_consumption.txt", header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+summary(data)
+
+#Subsetting the data from 2007-02-01 and 2007-02-02
+subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+globalActivePower <- as.numeric(subsetdata$Global_active_power)
+globalReactivePower <- as.numeric(subsetdata$Global_reactive_power)
+voltage <- as.numeric(subsetdata$Voltage)
+subMetering1 <- as.numeric(subsetdata$Sub_metering_1)
+subMetering2 <- as.numeric(subsetdata$Sub_metering_2)
+subMetering3 <- as.numeric(subsetdata$Sub_metering_3)
+
+#Create a plot for sub metering, plot3.png
+datetime <- strptime(paste(subsetdata$Date, subsetdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+
+#Saving plot
+dev.copy(png,"plot3.png", width=480, height=480)
+dev.off()
